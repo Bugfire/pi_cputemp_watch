34af17b6fb9e57d5f137396ac984e472bfffe005
bugfire <bugfire@users.noreply.github.com>
2015-06-09 22:33:59 +0900

    Get temp 20times and register average of them.

--- pi_cputemp_watch.js
+++ pi_cputemp_watch.js
@@ -9,14 +9,30 @@ var util = require('./util');
 
 var fs = require('fs');
 
-var text = fs.readFileSync(config.tempfile).toString();
-var temp = text / 1000.0;
+var get_temp = function() {
+  var text = fs.readFileSync(config.tempfile).toString();
+  return text / 1000.0;
+}
 
-var currentTime = util.getCurrentTime(config);
-var queries = [
-  'INSERT INTO ' + config.pdb + ' (Datetime,Val) VALUES ("' + currentTime + '",' + temp + ')'
-];
+var sampling = 20;
+var counter = sampling;
+var temp = 0;
 
-util.sendQueries(config, queries, function() {
-  process.exit();
-});
+var get_temp_x = function() {
+  counter--;
+  if (counter < 0) {
+    temp = temp / sampling;
+    var currentTime = util.getCurrentTime(config);
+    var queries = [
+      'INSERT INTO ' + config.pdb + ' (Datetime,Val) VALUES ("' + currentTime + '",' + temp + ')'
+    ];
+    util.sendQueries(config, queries, function() {
+      process.exit();
+    });
+  } else {
+     temp += get_temp();
+     setTimeout(get_temp_x, 250);
+  }
+}
+
+get_temp_x();
